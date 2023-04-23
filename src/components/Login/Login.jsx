2733f54416b987c7ab4c1f031c7a8c40405285e7
Aditta Chakraborty <adittacse@gmail.com>
2023-04-24 01:56:32 +0600

    login and registration page design updated

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,15 +2,22 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const Login = () => {
+    const handleLogin = (event) => {
+        event.preventDefault();
+        
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+    }
+    
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
+            <div className="hero-content flex-col">
+                <div className="text-center">
                     <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">New To Website? Please <Link to="/register">Register</Link></p>
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                <div className="card mt-3 flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -24,6 +31,9 @@ const Login = () => {
                             <input type="password" name="password" placeholder="password" required className="input input-bordered" />
                             <label className="label">
                                 <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                            <label className="label">
+                                <a href="/register" className="label-text-alt link link-hover">New To Website? Please Register!</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,15 +2,29 @@ import React from 'react';
 import {Link} from "react-router-dom";
 
 const Register = () => {
+    const handleRegister = (event) => {
+        event.preventDefault();
+        
+        const form = event.target;
+        const name = form.name.value;
+        const email = form.email.value;
+        const password = form.password.value;
+    }
+    
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
+            <div className="hero-content flex-col">
+                <div className="text-center">
                     <h1 className="text-5xl font-bold">Register now!</h1>
-                    <p className="py-6">Already Have An Account? Please <Link to="/login">Login</Link></p>
                 </div>
-                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <form className="card-body">
+                <div className="card mt-3 flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="name" required className="input input-bordered" />
+                        </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
@@ -23,7 +37,7 @@ const Register = () => {
                             </label>
                             <input type="password" name="password" placeholder="password" required className="input input-bordered" />
                             <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                                <a href="/login" className="label-text-alt link link-hover">Already Have An Account? Login Here!</a>
                             </label>
                         </div>
                         <div className="form-control mt-6">
