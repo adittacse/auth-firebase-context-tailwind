8dcfe8d73c314802eea6017f7bff72f0c29614c6
Aditta Chakraborty <adittacse@gmail.com>
2023-04-24 21:51:25 +0600

    auth providers added

---
+++ src/providers/__steps__.js
@@ -0,0 +1,12 @@
+/**
+ * CONTEXT API
+ * 1. create context (export)
+ * 2. create provider
+ * 3. set context value
+ *4. to access the value: useContext hook
+ */
+
+/**
+ * 1. set AuthProviders in main.jsx
+ * 2. inside provider access children props & then use
+ */
